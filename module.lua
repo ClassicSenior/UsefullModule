ca958db4003250c1ccc0422675b3e40d562971e0
ClassicSenior <107750710+ClassicSenior@users.noreply.github.com>
2024-12-09 22:10:21 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,4 +1,24 @@
 local usefullmodule = {}
+usefullmodule.__index = function() end
+
+local uncFuncs = {
+    setclipboard()
+}
+
+task.spawn()
+    local nonworkingfuncs = {}
+    for i,v in pairs(uncFuncs) do
+        local success, err = pcall(v)
+
+        if err then
+            table.insert(nonworkingfuncs, v)
+        end
+    end
+
+    if nonworkingfuncs[1] ~= nil then
+        return warn('Your executor does not support UsefullModule!')
+    end
+end
 
 function usefullmodule:offsetForward(args, speaker)
     local character = game.Players.LocalPlayer.Character
@@ -11,7 +31,11 @@ function usefullmodule:copyHwid()
     local fpbody = http_request({Url = 'https://httpbin.org/get', Method = 'GET'}).Body
     local fpdecoded = HttpService:JSONDecode(fpbody)
     local hwid = fpdecoded.headers[exec.."-Fingerprint"]
-    setclipboard(hwid)
+    setclipboard
+end
+
+function usefullmodule:copyHwid()
+
 end
 
 return usefullmodule
